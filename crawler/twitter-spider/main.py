311bd2c63172f30a7c6bc60045aee2528473a511
山南 <zhangpeichaook@163.com>
2020-02-08 01:16:19 +0800

    #twitter-spider

--- crawler/twitter-spider/main.py
+++ crawler/twitter-spider/main.py
@@ -3,17 +3,12 @@
 import configparser
 import os
 import sqlite3
-import sys
 import time
 from multiprocessing import Pool
 
-import requests
-from twitter_scraper import get_tweets
-from twitterscraper.query import query_tweets_from_user
-
 import init
-import utils.os.path
 import utils.runtime
+from spider import TwitterSpider
 
 config = configparser.ConfigParser()
 config.read(os.path.abspath('./config.ini'))
@@ -23,11 +18,39 @@ StorePath = config.get('settings', 'store_path')
 CrawlInterval = config.getint('settings', 'crawl_interval')
 Database = config.get('settings', 'database')
 
+
+def crawl(config):
+    spider = TwitterSpider(config, SavePath, StorePath, Database)
+    spider.start()
+
+
 if __name__ == '__main__':
     print('Spider starts at %s' % (time.strftime("%Y-%m-%d %H:%M:%S")))
     start = time.time()
 
-    # TODO
+    try:
+        con = sqlite3.connect(Database)
+        cur = con.cursor()
+
+        twitters = []
+        cur.execute(
+            'SELECT user,crawl_all FROM twitter_blog ORDER BY update_time DESC')
+        for row in cur.fetchall():
+            twitters.append({
+                'user': row[0],
+                'crawl_all': bool(row[1])
+            })
+
+        pool = Pool(processes=5)
+        pool.map(crawl, twitters)
+        pool.close()
+        pool.join()
+
+    finally:
+        if cur:
+            cur.close()
+        if con:
+            con.close()
 
     end = time.time()
     print('Spider finishes, run %s seconds.' % (end - start))
