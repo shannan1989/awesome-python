2569c2236aeaf88f17a62745a22e15a72abc44bc
山南 <zhangpeichaook@163.com>
2020-04-14 23:58:12 +0800

    #改用shutils

--- crawler/tumblr-spider/main.py
+++ crawler/tumblr-spider/main.py
@@ -6,8 +6,8 @@ import sqlite3
 import time
 from multiprocessing import Pool
 
-import init
-import utils.runtime
+from shutils import runtime
+
 from spider import TumblrSpider
 
 config = configparser.ConfigParser()
@@ -61,4 +61,4 @@ if __name__ == '__main__':
         "%Y-%m-%d %H:%M:%S", time.localtime(end + CrawlInterval))
     print('Spider will restart after %ss, at %s.' %
           (CrawlInterval, restart_time))
-    utils.runtime.restart(delay=CrawlInterval)
+    runtime.restart(delay=CrawlInterval)
