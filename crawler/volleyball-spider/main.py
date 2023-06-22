0fd3905f229865c4103967ca4f02ff8063776623
山南 <zhangpeichaook@163.com>
2023-06-22 13:48:46 +0800

    #排球爬虫主程序

---
+++ crawler/volleyball-spider/main.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+import time
+
+from shutils import runtime
+from shutils.settings import Settings
+from spider import VolleyballSpider, VolleyChinaSpider
+
+config = Settings.instance()
+CrawlInterval = config.getint('settings', 'crawl_interval')
+HoundUrl = config.get('settings', 'hound_url')
+
+if __name__ == '__main__':
+    start_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    print('Spider starts at %s' % (start_time))
+
+    start = time.time()
+
+    VolleyballSpider.houndUrl = HoundUrl
+    VolleyChinaSpider().start()
+
+    end = time.time()
+
+    print('Spider finishes, run %s seconds.' % (end - start))
+
+    restart_time = time.strftime(
+        "%Y-%m-%d %H:%M:%S", time.localtime(end + CrawlInterval))
+    print('Spider will restart after %ss, at %s.' %
+          (CrawlInterval, restart_time))
+
+    runtime.restart(delay=CrawlInterval)
